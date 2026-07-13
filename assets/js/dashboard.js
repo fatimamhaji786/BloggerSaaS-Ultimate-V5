9fadca934cabdc7163ded6606b6551f704b88e0d
fatimamhaji786 <130840155+fatimamhaji786@users.noreply.github.com>
2026-07-13 13:38:11 +0530

    Create dashboard.js

---
+++ assets/js/dashboard.js
@@ -0,0 +1,25 @@
+/*
+===========================================
+Dashboard
+===========================================
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Dashboard Loaded");
+
+    loadDashboard();
+
+});
+
+function loadDashboard(){
+
+    console.log("Loading Enterprise Dashboard...");
+
+}
+
+function logout(){
+
+    alert("Logout will be connected with Firebase Authentication.");
+
+}
