d1a564e199fc6b64b0b3e43c9c1f0c30048dd8d0
fatimamhaji786 <130840155+fatimamhaji786@users.noreply.github.com>
2026-07-13 13:52:54 +0530

    Create auth.js

---
+++ firebase/auth.js
@@ -0,0 +1,37 @@
+/*
+===========================================
+BloggerSaaS Ultimate V5 Enterprise
+Authentication
+===========================================
+*/
+
+// This file will contain:
+// - Login
+// - Logout
+// - Forgot Password
+// - Session Check
+// - Route Protection
+
+function login(email, password) {
+
+    console.log("Login:", email);
+
+    // Firebase Authentication code will be added here.
+
+}
+
+function logout() {
+
+    console.log("Logout");
+
+    // Firebase Sign Out
+
+}
+
+function checkAuth() {
+
+    console.log("Checking user session...");
+
+    // Firebase Auth State Listener
+
+}
