bc62de1dea167adb63e5ced84e01bc4b546e464f
fatimamhaji786 <130840155+fatimamhaji786@users.noreply.github.com>
2026-07-13 13:51:40 +0530

    Create firebase-config.js

---
+++ firebase/firebase-config.js
@@ -0,0 +1,22 @@
+/*
+===========================================
+BloggerSaaS Ultimate V5 Enterprise
+Firebase Configuration
+===========================================
+*/
+
+// Import Firebase SDKs later if using ES modules.
+// For now, paste your Firebase project configuration below.
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Firebase initialization will be added after the SDK is connected.
+
+// const app = firebase.initializeApp(firebaseConfig);
